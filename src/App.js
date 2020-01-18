8cbe3f6fc010ff4d3a78e1aa6b9720be6ab15bb7
Balaji Venkatraman <balaji@moonraft.com>
2020-01-18 18:04:23 +0530

    added useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function expensiveInitialState() {
+  return 100;
+}
+
+const App = () => {
+  const [count, setCount] = useState(10);
+  const [anotherCount, setAnotherCount] = useState(() =>
+    expensiveInitialState()
+  );
+  const [{ objCount1, objCount2 }, setObjCounts] = useState({
+    objCount1: 10,
+    objCount2: 20
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {count}, {anotherCount}
+      <br />
+      <button onClick={() => setCount(count + 1)}>ADD</button>
+      <button onClick={() => setAnotherCount(currentCount => currentCount - 1)}>
+        SUBTRACT
+      </button>
+      <br />
+      <p>Obj Count1: {objCount1} </p>
+      <p>Obj Count2: {objCount2}</p>
+      <button
+        onClick={() =>
+          setObjCounts(currentState => ({
+            ...currentState,
+            objCount1: currentState.objCount1 + 1
+          }))
+        }
+      >
+        + obj Count1
+      </button>
+      <button
+        onClick={() =>
+          setObjCounts(currentState => ({
+            ...currentState,
+            objCount2: currentState.objCount2 + 1
+          }))
+        }
+      >
+        + obj Count2
+      </button>
+      <button
+        onClick={() => {
+          setCount(c => c + 1);
+          setAnotherCount(c => c + 1);
+        }}
+      >
+        Change All
+      </button>
     </div>
   );
-}
+};
 
 export default App;
