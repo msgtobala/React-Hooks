da9287f6ee51b9b9db6f449c4d0a9b0953301d1f
Balaji Venkatraman <balaji@moonraft.com>
2020-01-20 12:36:31 +0530

    added multiple useEffect

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [email, setEmail] = useState('');
   useEffect(() => {
-    console.log('mounted / render');
-    return () => {
-      console.log('cleaned-up')
-    }
-  }, [email]);
+    console.log('mount1');
+  });
+  useEffect(() => {
+    console.log('mount2');
+  });
   return (
     <div>
       <input
